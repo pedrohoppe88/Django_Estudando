1db7c7c7bd8863de505d7eb07f9f2eb641be2b9b
pedrohoppe88 <pedrohhoopp@gmail.com>
2025-06-15 19:05:30 -0300

    gyuigYIG

--- meuprojeto/todos/models.py
+++ meuprojeto/todos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    titulo = models.CharField(max_length=100, null=False, blank=False)  # Título da tarefa
+    concluido = models.BooleanField(default=False)  # Se está concluída ou não
+    criado_at = models.DateTimeField(auto_now_add=True)  # Data de criação
+    deadline = models.DateTimeField(null=True, blank=True)  # Data limite para conclusão
+    finished = models.DateTimeField(null=True, blank=True)  # Data de conclusão
+
+    def __str__(self):
+        return self.titulo
