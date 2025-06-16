4388cce1f6f0844e16dac354410cab3047f02860
pedrohoppe88 <pedrohhoopp@gmail.com>
2025-06-15 22:15:00 -0300

    views usuaros

--- meuprojeto/todos/models.py
+++ meuprojeto/todos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Users(models.Model):
+class Usuarios(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     criado_em = models.DateTimeField(auto_now_add=True)
@@ -9,12 +9,13 @@ class Users(models.Model):
         return self.nome
 
 class Todo(models.Model):
+    id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=100, null=False, blank=False)
     concluido = models.BooleanField(default=False)
     criado_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
     finished = models.DateTimeField(null=True, blank=True)
-    user = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='todos')
+    user = models.ForeignKey(Usuarios, on_delete=models.CASCADE, related_name='todos')
 
     def __str__(self):
         return self.titulo
--- meuprojeto/todos/views.py
+++ meuprojeto/todos/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from .models import Todo
+from .models import Usuarios
 
 def home(request):
     Todos = Todo.objects.all()  # Obtém todas as tarefas do banco de dados
     return render(request, 'todos/index.html', {"todos": Todos})  # Renderiza o template com as tarefas
+
+def usuarios(request):
+    #salvar os dados da tela para o banco de dados
+    novoUsuario = Usuarios()
+    request.method = 'POST'
+    novoUsuario = request.POST.get('email')
+    novoUsuario = request.POST.get('password')
+    
+    # exibir os user já cadastrados em uma tela
+    
+    usuarios = {
+        'usuarios': Usuarios.objects.all()  # Obtém todos os usuários do banco de dados 
+        
+    }
+    #retornar os dados para a página HTML
+    return render(request, 'todos/usuarios.html', usuarios)
+    
+    
+    #nome = request.POST.get('email')
+    #email = request.POST.get('password')
